39a2c6246785424d8de25af3c91fee752b611ff9
Kamae <luceropintos@abc.gob.ar>
2023-02-23 12:52:43 -0300

    edit task through modal ready

--- Home.tsx
+++ Home.tsx
@@ -1,34 +1,15 @@
-import {
-  Alert,
-  FlatList,
-  NativeSyntheticEvent,
-  StatusBar,
-  TextInputChangeEventData,
-} from "react-native";
-import { Pressable, StyleSheet, TextInput, View } from "react-native";
+import { CameraRollAssetInfo, FlatList, StatusBar } from "react-native";
+import { StyleSheet, View } from "react-native";
 import { useState } from "react";
-import Icon from "react-native-vector-icons/Feather";
 import ListItem from "./components/ListItem";
+import TaskInput from "./components/TextInput";
+import EditTaskModal from "./components/EditTaskModal";
 
 const Home: React.FC = () => {
-  const [textValue, setTextValue] = useState<string>("");
   const [tasksList, setTasksList] = useState<string[]>([]);
+  const [modalState, setModalState] = useState<boolean>(false);
+  const [taskToEdit, setTaskToEdit] = useState<string>("");
 
-  const onChangeHandler = (
-    event: NativeSyntheticEvent<TextInputChangeEventData>
-  ): void => {
-    setTextValue(event.nativeEvent.text);
-  };
-
-  const addTask: VoidFunction = (): void => {
-    if (!textValue) {
-      return Alert.alert("New Task", "Task cannot be empty!");
-    } else if (tasksList.includes(textValue)) {
-      return Alert.alert("New Task", "Tasks cannot be repeated!");
-    }
-    setTasksList((tasksList) => [...tasksList, textValue]);
-    setTextValue("");
-  };
   const deleteTask: CallableFunction = (taskName: string): void => {
     setTasksList((taskList) =>
       taskList.filter((taskElement) => taskElement != taskName)
@@ -39,23 +20,32 @@ const Home: React.FC = () => {
     <View style={styles.rootContainer}>
       <StatusBar backgroundColor={"#ffc8dd"} />
       <View style={styles.inputContainer}>
-        <TextInput
-          onChange={onChangeHandler}
-          value={textValue}
-          style={styles.textInput}
-        />
-        <Pressable style={styles.addIconContainer} onPress={addTask}>
-          <Icon style={styles.addIcon} name="plus" />
-        </Pressable>
+        <TaskInput tasksList={tasksList} setTasksList={setTasksList} />
       </View>
       <View style={styles.tasksContainer}>
         <FlatList
           data={tasksList}
           renderItem={(item) => (
-            <ListItem id={item.item} text={item.item} deleteTask={deleteTask}  />
+            <ListItem
+              id={item.item}
+              text={item.item}
+              deleteTask={deleteTask}
+              modalOpenHandler={() => {
+                setModalState(true);
+                setTaskToEdit(item.item)
+              }}
+            />
           )}
         />
       </View>
+      <EditTaskModal
+        modalState={modalState}
+        modalCloseHandler={() => {
+          setModalState(false);
+        }}
+        taskToEdit={taskToEdit}
+        setTasksList={setTasksList}
+      />
     </View>
   );
 };
@@ -68,26 +58,7 @@ const styles = StyleSheet.create({
   },
   inputContainer: {
     flexDirection: "row",
-
     alignItems: "center",
-  },
-  textInput: {
-    flex: 1,
-    fontSize: 16,
-    fontWeight: "300",
-    padding: "1%",
-    borderRadius: 5,
-    backgroundColor: "#fff",
-  },
-  addIconContainer: {
-    backgroundColor: "#bde0fe",
-    marginLeft: "2%",
-    padding: "1%",
-    borderRadius: 5,
-    elevation: 2,
-  },
-  addIcon: {
-    fontSize: 30,
   },
   tasksContainer: { flex: 1, marginTop: "5%" },
 });
--- components/EditTaskModal.tsx
+++ components/EditTaskModal.tsx
@@ -1,19 +1,56 @@
-import { Text, View, Modal, StyleSheet, TextInput, Button } from "react-native";
+import {
+  Text,
+  View,
+  Modal,
+  StyleSheet,
+  TextInput,
+  Button,
+  NativeSyntheticEvent,
+  TextInputChangeEventData,
+} from "react-native";
 import { useState } from "react";
 
 type ModalPropTypes = {
   modalState: boolean;
   modalCloseHandler: CallableFunction;
+  setTasksList: CallableFunction;
+  taskToEdit: string;
 };
 
-function EditTaskModal({ modalState, modalCloseHandler }: ModalPropTypes) {
+function EditTaskModal({
+  modalState,
+  modalCloseHandler,
+  setTasksList,
+  taskToEdit,
+}: ModalPropTypes) {
+  const [inputValue, setInputValue] = useState("");
+  const onChangeHandler = (
+    event: NativeSyntheticEvent<TextInputChangeEventData>
+  ): void => {
+    setInputValue(event.nativeEvent.text);
+  };
   return (
     <Modal transparent={true} animationType="fade" visible={modalState}>
       <View style={styles.modalContainer}>
         <View style={styles.modal}>
-          <TextInput style={styles.textInput} placeholder="Type in.." />
+          <TextInput
+            style={styles.textInput}
+            placeholder="Type in.."
+            onChange={onChangeHandler}
+            value={inputValue}
+          />
           <View style={styles.modalButtonera}>
-            <Button  title={"Accept"} />
+            <Button
+              title={"Accept"}
+              onPress={() => {
+                setTasksList((taskList: string[]) => {
+                  taskList[taskList.indexOf(taskToEdit)] = inputValue;
+                  return taskList;
+                });
+                setInputValue("");
+                modalCloseHandler();
+              }}
+            />
             <Button
               title={"Cancel"}
               onPress={() => {
@@ -42,6 +79,10 @@ const styles = StyleSheet.create({
     paddingVertical: "8%",
   },
   textInput: { marginVertical: "3%", fontSize: 17 },
-  modalButtonera: { flexDirection: "row", justifyContent:'space-between', width:'50%' },
+  modalButtonera: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    width: "50%",
+  },
 });
 export default EditTaskModal;
